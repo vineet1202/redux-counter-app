a430a9c7b650a9a6946782769f53b9299c516d9e
vineet1202 <vineetloktak@gmail.com>
2023-10-24 13:28:11 +0530

    Add to remote Github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch({ type: "INC" });
+  };
+  const decrement = () => {
+    dispatch({ type: "DEC" });
+  };
+
+  const Add = () => {
+    dispatch({ type: "ADD", payload: 10 });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={Add}>Add by 10</button>
     </div>
   );
 }
